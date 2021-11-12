c8806c7f08c8eebf1123cead5b8f7ba9adf37830
ocan00cemal <ocancemal1996@gmail.com>
2021-11-12 23:20:05 +0300

    Easy

---
+++ 119. Pascal's Triangle.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getRow(self, rowIndex: int):
+        if rowIndex == 0:
+            return [1]
+
+        cur_triangle = [[1], [1, 1]]
+
+        for i in range(2, rowIndex + 1):
+            cur_row = [1]
+            j = i - 1
+            for internal in range(j):
+                cur_row.append(
+                    cur_triangle[j][internal] + cur_triangle[j][internal + 1]
+                )
+            cur_row.append(1)
+            cur_triangle.append(cur_row)
+
+        return cur_triangle[-1]
