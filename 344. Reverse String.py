a2cb8342d093a7aff412a2753b3b907c13881dc2
ocan00cemal <ocancemal1996@gmail.com>
2021-11-13 19:20:54 +0300

    Easy

---
+++ 344. Reverse String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        n = len(s)
+
+        for i in range(n // 2):
+            s[i], s[-(i + 1)] = s[-(i + 1)], s[i]
+
+        """
+        Do not return anything, modify s in-place instead.
+        """
