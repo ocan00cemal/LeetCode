768abe6748696198e654e259cbe59437e34f20b6
ocan00cemal <ocancemal1996@gmail.com>
2021-11-14 19:12:30 +0300

    Easy

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -1,43 +1,32 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-
 class Solution:
-    def addTwoNumbers(
-        self, l1: Optional[ListNode], l2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        head = cur = ListNode(0)
-        increase = 0
+    def addTwoNumbers(self, l1, l2):
+        head = temp = ListNode(0)
+        remainder = 0
 
-        while l1 != None and l2 != None:
-            if l1.val + l2.val + increase >= 10:
-                cur.next = ListNode((l1.val + l2.val + increase) % 10)
-                increase = 1
+        while l1 and l2:
+            if l1.val + l2.val + remainder >= 10:
+                temp.next = ListNode((l1.val + l2.val + remainder) % 10)
+                remainder = 1
             else:
-                cur.next = ListNode(l1.val + l2.val + increase)
-                increase = 0
-
+                temp.next = ListNode(l1.val + l2.val + remainder)
+                remainder = 0
             l1 = l1.next
             l2 = l2.next
-            cur = cur.next
+            temp = temp.next
+     
+        long_linked = l1 or l2
 
-        if l1 != None or l2 != None:
-            l_remain = l1 or l2
-            while l_remain != None:
-                if l_remain.val + increase >= 10:
-                    cur.next = ListNode((l_remain.val + increase) % 10)
-                    increase = 1
-                else:
-                    cur.next = ListNode(l_remain.val + increase)
-                    increase = 0
+        while long_linked:
+            if long_linked.val + remainder >= 10:
+                temp.next = ListNode((long_linked.val + remainder) % 10)
+                remainder = 1
+            else:
+                temp.next = ListNode(long_linked.val + remainder)
+                remainder = 0
+            long_linked = long_linked.next
+            temp = temp.next
 
-                l_remain = l_remain.next
-                cur = cur.next
-
-        if increase:
-            cur.next = ListNode(increase)
+        if remainder:
+            temp.next = ListNode(remainder)
 
         return head.next
