0447cfa651200a1b3d4d2378fc8e38749010adc6
ocan00cemal <ocancemal1996@gmail.com>
2021-11-15 21:17:55 +0300

    Easy

--- 58. Length of Last Word.py
+++ 58. Length of Last Word.py
@@ -2,14 +2,15 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         counter = 0
         i = -1
-        while True:
-            try:
-                if s[i] == " " and counter == 0:
-                    i -= 1
-                elif s[i] != " ":
-                    counter += 1
-                    i -= 1
-                elif s[i] == " " and counter != 0:
-                    return counter
-            except:
-                return counter
+        n = len(s)
+
+        while abs(i) <= n:
+            if s[i] == " " and counter == 0:
+                i -= 1
+            elif s[i] != " ":
+                counter += 1
+                i -= 1
+            elif s[i] == " " and counter != 0:
+                break
+
+        return counter
